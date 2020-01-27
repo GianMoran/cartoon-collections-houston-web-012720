fe718b6d97af387a9a9f489ba7caddca617f8cb0
Gian Moran <gpmoran_@outlook.com>
2020-01-27 19:50:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,9 @@ def long_planeteer_calls(array_of_calls)
    end
 end
 
-def find_the_cheese(arr_of_str)
+def find_the_cheese
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
   
 end
