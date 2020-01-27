503659b88200abb0ee262c2b582703018229d437
Gian Moran <gpmoran_@outlook.com>
2020-01-27 19:11:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each do |name_of_dwarf|
+    puts name_of_dwarf
+  end
 end
 
 def summon_captain_planet# code an argument here
